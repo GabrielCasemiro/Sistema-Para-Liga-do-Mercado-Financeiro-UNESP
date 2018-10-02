c8399c87437ddb620efcc1e16b736c7205a2a1c8
Gabriel <gabrielcasemiro68@gmail.com>
2018-10-02 20:04:31 -0300

    data desc

--- website/views.py
+++ website/views.py
@@ -67,7 +67,7 @@ def categoria_editorial(request):
     return render(request, 'category.html',{'posts':postagens,'titulo':titulo})
 
 def categoria_artigos(request):
-    posts = Post.objects.filter(tipo_post='Artigos').order_by('data')
+    posts = Post.objects.filter(tipo_post='Artigos').order_by('-data')
     titulo = "Artigos"
     page = request.GET.get('page', 1)
     paginator = Paginator(posts, 10)
@@ -81,7 +81,7 @@ def categoria_artigos(request):
     return render(request, 'category.html',{'posts':postagens,'titulo':titulo})
 
 def categoria_destaques(request):
-    posts = Post.objects.filter(tipo_post='Destaques da Semana').order_by('data')
+    posts = Post.objects.filter(tipo_post='Destaques da Semana').order_by('-data')
     titulo = "Destaques da Semana"
     page = request.GET.get('page', 1)
     paginator = Paginator(posts, 10)
@@ -95,7 +95,7 @@ def categoria_destaques(request):
     return render(request, 'category.html',{'posts':postagens,'titulo':titulo})
 
 def categoria_cursos(request):
-    posts = Post.objects.filter(tipo_post='Cursos e Eventos').order_by('data')
+    posts = Post.objects.filter(tipo_post='Cursos e Eventos').order_by('-data')
     titulo = "Cursos e Eventos"
     page = request.GET.get('page', 1)
     paginator = Paginator(posts, 10)
@@ -111,7 +111,7 @@ def categoria_cursos(request):
 def pesquisa(request):
     tipo_categoria = request.POST.get('tipo_categoria','Editorial')
     texto_pesquisa = request.POST.get('texto_pesquisa','')
-    posts = Post.objects.filter(Q(tipo_post=tipo_categoria, titulo__icontains=strip_accents(texto_pesquisa))|Q(tipo_post=tipo_categoria, descricao__icontains=strip_accents(texto_pesquisa))).order_by('data')
+    posts = Post.objects.filter(Q(tipo_post=tipo_categoria, titulo__icontains=strip_accents(texto_pesquisa))|Q(tipo_post=tipo_categoria, descricao__icontains=strip_accents(texto_pesquisa))).order_by('-data')
     
     page = request.GET.get('page', 10)
     paginator = Paginator(posts, 10)
@@ -137,7 +137,7 @@ def page(request, num="1"):
     except:
         pass
     #Post_popular
-    post_popular = Post.objects.all().order_by('data')[:8]
+    post_popular = Post.objects.all().order_by('-data')[:8]
     return render(request, 'post-page.html',{'post':post[0],'post_popular':post_popular})
 
 def enviar_email(request):
